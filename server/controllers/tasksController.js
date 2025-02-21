142fb8422781125c51e3957176f87c853e43c798
EderAJ30 <EAJ30092004@outlook.com>
2025-02-21 17:35:52 -0600

    se suube update

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -43,8 +43,16 @@ export const createTask = async (req, res) => {
   }
 };
 
-export const updateTask = (req, res) => {
-  res.send("actualizando tarea");
+export const updateTask = async (req, res) => {
+  try {
+    const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
+      req.body,
+      req.params.id,
+    ]);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const deleteTask = async (req, res) => {
